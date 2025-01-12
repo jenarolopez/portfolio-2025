a6d3f735e11207baf3ce212bdde8b9a85c7f1396
jenarolopez <47820660+jenarolopez@users.noreply.github.com>
2025-01-12 13:24:34 +0800

    fix spotlight

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
+import { useEffect, useState } from "react";
 import LeftColumn from "./layout/LeftColumn";
 import MobileNav from "./layout/MobileNav";
 import RightColumn from "./layout/RightColumn";
 import Body from "./layout/Body";
 
 export default function App() {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const updateMousePosition = (ev) => {
+      setMousePosition({
+        x: ev.clientX + window.scrollX,
+        y: ev.clientY + window.scrollY,
+      });
+    };
+
+    window.addEventListener("mousemove", updateMousePosition);
+
+    return () => {
+      window.removeEventListener("mousemove", updateMousePosition);
+    };
+  }, []);
+
   return (
-    <div className="bg-slate-900">
+    <div className="relative bg-slate-900">
+      <div
+        className="pointer-events-none fixed top-0 inset-0 z-30 transition duration-300 lg:absolute w-full bg-red-100"
+        style={{
+          background: `radial-gradient(600px at ${mousePosition.x}px ${mousePosition.y}px, rgba(29, 78, 216, 0.15), transparent 80%)`,
+        }}
+      ></div>
       <Body>
         <LeftColumn />
         <RightColumn />
       </Body>
     </div>
-  )
+  );
 }
-
--- src/layout/Body.js
+++ src/layout/Body.js
@@ -1,28 +1,9 @@
 import { useState, useEffect } from "react";
 
 const Body = (props) => {
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-
-  useEffect(() => {
-    const updateMousePosition = (ev) => {
-      setMousePosition({ x: ev.clientX, y: ev.clientY });
-    };
-
-    window.addEventListener("mousemove", updateMousePosition);
-
-    return () => {
-      window.removeEventListener("mousemove", updateMousePosition);
-    };
-  }, []);
 
   return (
     <div className="bg-slate-900 mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-16 lg:py-0">
-      <div
-        className="pointer-events-none fixed top-0 inset-0 z-30 transition duration-300 lg:absolute w-full"
-        style={{
-          background: `radial-gradient(600px at ${mousePosition.x}px ${mousePosition.y}px, rgba(29, 78, 216, 0.15), transparent 80%)`,
-        }}
-      ></div>
       <div className="lg:flex lg:justify-between lg:gap-4 text-slate-100">
         {props.children}
       </div>
