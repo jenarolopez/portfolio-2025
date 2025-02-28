d1e0aef54006e7c4993c99d40417ebb446f86b11
jenarolopez <jenjenlopez37@gmail.com>
2025-03-01 03:01:18 +0800

    Implement dynamic Archive page with project data from JSON

--- src/App.js
+++ src/App.js
@@ -54,9 +54,9 @@ export default function App() {
                   background: `radial-gradient(600px at ${mousePosition.x}px ${mousePosition.y}px, rgba(29, 78, 216, 0.15), transparent 80%)`,
                 }}
               ></div>
-              <Body>
+             
                 <Archive />
-              </Body>
+              
             </div>
           }
         />
